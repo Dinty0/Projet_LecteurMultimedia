b41379c6cdd5722ce7924d113c436c1a521a0603
Kevin ESPASA <E114942M@I019V7pc14.ensinfo.sciences.univ-nantes.prive>
2015-11-19 18:57:15 +0100

    video

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,35 @@
 #include <SFML/Graphics.hpp>
 #include <TGUI/TGUI.hpp>
 #include <sfeMovie/Movie.hpp>
+#include "dirent.h"
 #define THEME_CONFIG_FILE "src/widgets/Black.conf"
 /*
 #include "AbstractFactory/video.hpp"
 #include "AbstractFactory/audio.hpp"
 #include "AbstractFactory/image.hpp"
 */
+   
+
+
 int main()
 {    
     sf::RenderWindow window(sf::VideoMode(600, 100), "Lecteur multimedia");
     tgui::Gui gui(window);
     tgui::Callback callback;
-    sfe::Movie movie; 
+    sfe::Movie movie;
+    
+    Dir *dir=NULL;
+    struct dirent *file;
+    
     movie.openFromFile("mobile.ogg");
-    movie.resizeToFrame(0, 0, 600, 450);
 	window.setVerticalSyncEnabled(true);     
-
-
+	
     if (gui.setGlobalFont("src/fonts/DejaVuSans.ttf") == false)
         return 1;
 
     tgui::Picture::Ptr picture(gui);
     picture->load("src/fond-blanc.png");
-
+    
     tgui::Button::Ptr buttonVideo(gui);
     buttonVideo->load(THEME_CONFIG_FILE);
     buttonVideo->setPosition(0, 0);
@@ -76,11 +82,20 @@ int main()
             }
             else if (callback.id == 3)
             {
-              //  Image img;
-            //    img.afficher();
-             //   img.run();
-            }
-    }
+				gui.removeAllWidgets();
+				dir=opendir("Ressource\\Image\\");
+				window.create(sf::VideoMode(800, 600), "dossier");
+				tgui::ListBox::Ptr listBox(gui);
+				listBox->load(THEME_CONFIG_FILE);
+				listBox->setSize(120, 21);
+				listBox->setPosition(210, 440);
+				while( (file=readdir(dir)) !=NULL)
+				{
+					listBox->addItem(file->d_name);			
+				}
+				
+			}
+    } 
     window.clear();
                     window.draw(movie);
 
